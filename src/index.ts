2644ac9d45656a2137909e405bfd9d558e643dd8
legiorex <batyushev@gmail.com>
2022-07-22 22:10:48 +0300

    fix evn

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import dotenv from 'dotenv'
 import express, { Express, Request, Response } from 'express'
 
+dotenv.config()
+
 const app: Express = express()
 const port = process.env.PORT
 
-dotenv.config()
-
 app.get('/', (request: Request, response: Response) => {
-    response.send('Hello world!')
+    response.send('Hello world!!!!!!!!!!!!!!')
 })
 app.listen(port, () => console.log(`⚡️ Running on port ${port}`))
