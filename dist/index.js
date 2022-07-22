9a0246b908ba19bc523b5ac56d124c8600fbcaa8
legiorex <batyushev@gmail.com>
2022-07-22 21:35:46 +0300

    add eslint and prettier

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv';
+import express from 'express';
 const app = express();
 const port = process.env.PORT;
-app.get("/", (request, response) => {
-    response.send("Hello world!");
+dotenv.config();
+app.get('/', (request, response) => {
+    response.send('Hello world!');
 });
 app.listen(port, () => console.log(`⚡️ Running on port ${port}`));
 //# sourceMappingURL=index.js.map
