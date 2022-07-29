b3ec7707b500417a0a81d69d9cf7fc3d6550e3fa
legiorex <batyushev@gmail.com>
2022-07-29 14:06:54 +0300

    fix port

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename)
 const uploadsDir = path.join(__dirname, '/uploads/')
 
 dotenv.config()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
 
 mongoose
     .connect(`${process.env.DB_URL}`)
